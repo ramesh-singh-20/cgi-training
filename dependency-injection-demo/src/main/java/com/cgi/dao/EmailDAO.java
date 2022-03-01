c9f6390cc9ae6e12644da6c668fe1402aefe7e79
Singh <rame.singh@cgi.com>
2022-02-28 22:02:04 -0500

    Demo project for depedency injection.

---
+++ dependency-injection-demo/src/main/java/com/cgi/dao/EmailDAO.java
@@ -0,0 +1,23 @@
+package com.cgi.dao;
+
+import com.cgi.model.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailDAO {
+    private static List<String> emails;
+
+    static {
+        emails= new ArrayList<>();
+        Contact c1= new Contact("abc@gmail.com");
+        Contact c2= new Contact("def@gmail.com");
+        Contact c3= new Contact("xyz@gmail.com");
+    }
+
+
+    private static List<String> getEmails(){
+        //In real- world applications this list will be fetched from database.
+        return emails;
+    }
+}
---
+++ dependency-injection-demo/src/main/java/com/cgi/model/Contact.java
@@ -0,0 +1,17 @@
+package com.cgi.model;
+
+public class Contact {
+    private String email;
+
+    public Contact(String email){
+        this.email= email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
