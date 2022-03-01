b79be3cf4005e3fcd882873bcd1b2d12e32fc0d0
Singh <rame.singh@cgi.com>
2022-02-28 21:54:05 -0500

    Demo project for depedency injection.

---
+++ dependency-injection-demo/src/main/java/com/cgi/DependencyInjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+	}
+
+}
---
+++ dependency-injection-demo/src/test/java/com/cgi/DependencyInjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
