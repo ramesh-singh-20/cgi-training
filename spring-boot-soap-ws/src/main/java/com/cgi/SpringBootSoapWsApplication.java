bc1b35ed987ff02b79ab03d3a128333a3cdf3e4a
Singh <rame.singh@cgi.com>
2022-03-05 12:13:34 -0500

    Initial project setup.

---
+++ spring-boot-soap-ws/src/main/java/com/cgi/SpringBootSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWsApplication.class, args);
+	}
+
+}
