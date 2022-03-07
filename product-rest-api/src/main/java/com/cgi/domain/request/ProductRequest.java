8c25f769322bd8aa6e46d370c9c67e6213ecf24f
Singh <rame.singh@cgi.com>
2022-03-07 12:18:41 -0500

    Implementing rest api with postgres database.

--- product-rest-api/src/main/java/com/cgi/domain/request/ProductRequest.java
+++ product-rest-api/src/main/java/com/cgi/domain/request/ProductRequest.java
@@ -1,11 +1,14 @@
 package com.cgi.domain.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductRequest {
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
--- product-rest-api/src/main/java/com/cgi/domain/response/ProductResponse.java
+++ product-rest-api/src/main/java/com/cgi/domain/response/ProductResponse.java
@@ -1,10 +1,12 @@
 package com.cgi.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductResponse {
     private Long id;
     private String name;
