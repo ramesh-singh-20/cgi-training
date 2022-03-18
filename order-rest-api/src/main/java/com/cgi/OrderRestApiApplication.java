580cbf1b959fb06d44552db5ad530d1b757a4c30
Singh <rame.singh@cgi.com>
2022-03-18 08:07:12 -0400

    Initial project set up.

---
+++ order-rest-api/src/main/java/com/cgi/OrderRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRestApiApplication.class, args);
+	}
+
+}
---
+++ order-rest-api/src/main/java/com/cgi/entity/OrderEntity.java
@@ -0,0 +1,4 @@
+package com.cgi.entity;
+
+public class OrderEntity {
+}
