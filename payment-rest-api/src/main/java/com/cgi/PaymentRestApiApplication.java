b229f38d225c478f60ea3f6fa82d42c3c4177c35
Singh <rame.singh@cgi.com>
2022-03-20 18:59:37 -0400

    Added code for sevice discovery client.

--- payment-rest-api/src/main/java/com/cgi/PaymentRestApiApplication.java
+++ payment-rest-api/src/main/java/com/cgi/PaymentRestApiApplication.java
@@ -2,8 +2,10 @@ package com.cgi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentRestApiApplication {
 
 	public static void main(String[] args) {
