305ec14debabecc4a4b8c598b940d73ffc267222
Singh <rame.singh@cgi.com>
2022-03-20 16:50:49 -0400

    Creating Service Discovery server.

---
+++ ekart-service-registry/src/main/java/com/cgi/EkartServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ ekart-service-registry/src/test/java/com/cgi/EkartServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkartServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
